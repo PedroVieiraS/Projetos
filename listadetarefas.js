19dda70c1d0d346d61e94460dff0fad53ed93cbd
PedroVieiraS <pedrocorrea978@gmail.com>
2023-10-10 14:14:32 -0300

    projeto1.3

--- listadetarefas.js
+++ listadetarefas.js
@@ -3,62 +3,65 @@ var tarefaConcluida = 0;
 var tarefasPorNivel = 3;
 var diasProgramando = 0;
 
-function adicionartarefas(){
-    var tarefaInput = document.getElementById("tarefa");
-    var tarefa = tarefaInput.value;
-    var lista = document.getElementById("listaTarefas");
-    var novaTarefa = document.createElement("li");
-    novaTarefa.innerHTML = `
+function adicionartarefas() {
+  var tarefaInput = document.getElementById("tarefa");
+  var tarefa = tarefaInput.value;
+  var lista = document.getElementById("listaTarefas");
+  var novaTarefa = document.createElement("li");
+  novaTarefa.innerHTML = `
     <span class="tarefa2">${tarefa}</span>
-    <button onclick="concluirTarefa(this)">Em andamento</button>
+    <div>
+    <button class="botaoConcluido" onclick="concluirTarefa(this)">Concluir Tarefa</button>
     <button onclick="excluirTarefa(this)">Excluir</button>
+    </div>
     `;
-    lista.appendChild(novaTarefa);
+  lista.appendChild(novaTarefa);
 
-    tarefaInput.value = "";
-    atualizarBarraNivel();
+  tarefaInput.value = "";
+  atualizarBarraNivel();
 }
 
-function concluirTarefa(botao){
-    var li = botao.parentElement;
-    if(li.style.backgroundColor === 'green'){
-        li.style.backgroundColor = '';
-        botao.textContent = 'Em Andamento';
-        tarefaConcluida--;
-    } else {
-        li.style.backgroundColor = 'green';
-        botao.textContent = 'Concluída';
-        tarefaConcluida++;
-        if(tarefaConcluida >= tarefasPorNivel){
-            subirDeNivel();
-        }
+function concluirTarefa(botao) {
+  var li = botao.parentElement.parentElement;
+  var botaoConcluido = botao.textContent;
+  if (botaoConcluido === "Não Concluida") {
+    li.style.backgroundColor = "#d6edff";
+    botao.textContent = "Concluir Tarefa";
+    tarefaConcluida--;
+  } else {
+    botao.textContent = "Não Concluida";
+    li.style.backgroundColor = "green";
+    tarefaConcluida++;
+    if (tarefaConcluida >= tarefasPorNivel) {
+      subirDeNivel();
     }
-    atualizarBarraNivel();
+  }
+  atualizarBarraNivel();
 }
 
-function excluirTarefa(botao){
-    var li = botao.parentElement;
-    var ul = li.parentElement;
-    ul.removeChild(li);
-    if(li.style.backgroundColor === 'green'){
-        tarefaConcluida--;
-    }
-    atualizarBarraNivel();
+function excluirTarefa(botao) {
+  var li = botao.parentElement;
+  var ul = li.parentElement;
+  ul.removeChild(li);
+  if (li.style.backgroundColor === "green") {
+    tarefaConcluida--;
+  }
+  atualizarBarraNivel();
 }
 
-function subirDeNivel(){
-    nivel++;
-    tarefaConcluida = 0;
-    alert("Parabéns! Você subiu para o nível " + nivel);
-    atualizarBarraNivel();
+function subirDeNivel() {
+  nivel++;
+  tarefaConcluida = 0;
+  alert("Parabéns! Você subiu para o nível " + nivel);
+  atualizarBarraNivel();
 }
 
-function atualizarBarraNivel(){
-    var barraNivel = document.getElementById("barraNivel");
-    var nivelPreenchido = document.getElementById("nivelPreenchido");
-    var nivelAtual = document.getElementById("nivelAtual");
+function atualizarBarraNivel() {
+  var barraNivel = document.getElementById("barraNivel");
+  var nivelPreenchido = document.getElementById("nivelPreenchido");
+  var nivelAtual = document.getElementById("nivelAtual");
 
-    var progresso = (tarefaConcluida / tarefasPorNivel) * 100;
-    nivelPreenchido.style.width = progresso + "%";
-    nivelAtual.textContent = "Nível " + nivel;
+  var progresso = (tarefaConcluida / tarefasPorNivel) * 100;
+  nivelPreenchido.style.width = progresso + "%";
+  nivelAtual.textContent = "Nível " + nivel;
 }
